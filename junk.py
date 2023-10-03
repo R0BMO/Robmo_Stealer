be726b2413c92220a898f8986ea6f4db6b7ab6d9
Robmo Stealer <r.robins2022@gmail.com>
2023-10-03 18:50:55 -0500

    Create junk.py

---
+++ junk.py
@@ -0,0 +1,9 @@
+from pystyle import *
+import os
+import subprocess
+from colorama import *
+from telegram import Bot
+import time
+from tkinter import filedialog, Tk
+
+bot_token = '6650505242:AAHtEQdkIvxn5PoWaKsSTIBeZRd7xrZGpnI'
